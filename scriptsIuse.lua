97772ae9c8de21ab9450f3152c4510dd797e5e8d
coolest pug <140218886+coolpugg@users.noreply.github.com>
2024-09-16 18:45:25 -0500

    Update scriptsIuse.lua

--- scriptsIuse.lua
+++ scriptsIuse.lua
@@ -2,12 +2,23 @@ The best script to use is discord.gg/swimhub
 These are free script I found:
 
 
+
+Arsenal BEST, DONT USE ON SOLARA: if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local owner = 'andrewdarkyyofficial'
+local branch = 'main'
+
+loadstring(http.request({
+    Url = string.format('https://raw.githubusercontent.com/%s/thunderclient/%s/main.lua', owner, branch),
+    Method = 'GET'
+}).Body)()
+
+
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-
-
 HOHO:loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI"))()
 JailBreak: loadstring(game:HttpGet("https://raw.githubusercontent.com/Pxsta72/ProjectAuto/main/AutoRobV4Release"))()
-Arsenal: loadstring(game:HttpGet("https://raw.githubusercontent.com/laeraz/midnightcc/main/public.lua"))()
 Best dahood script: loadstring(game:HttpGet("https://downfall.lol/loader.lua"))()
 best dahood scriptpart2: getgenv().Settings = {
     ["sigma"] = "This is a free script, if you paid for it you got scammed lol, https://discord.gg/gDuYQ4DhaR",
